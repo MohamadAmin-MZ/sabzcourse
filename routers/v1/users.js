9d303b0a64adf89b8648941fbdae0af683b77e81
MohamadAmin-MZ <mohamadamonmohamadzadeh1@gmail.com>
2025-11-21 17:58:48 -0800

    Add admin authentication

--- routers/v1/users.js
+++ routers/v1/users.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userController = require("../../controllers/v1/user");
+const authMiddlewares = require("../../middlewares/auth")
+const isAdminMiddlewares = require("../../middlewares/isAdmin")
 
 const router = express.Router();
 
-router.post("/ban/:id", userController.banUser);
+router.post("/ban/:id", authMiddlewares.havingToken, isAdminMiddlewares.adminAuthentication, userController.banUser);
 
 module.exports = router;
